f4755afa489ff0b4b91b1ad0c9e77a906fdefac4
codename-carter <tioncarter@gmail.com>
2023-08-21 19:33:55 -0400

    set up team dev enviornment

---
+++ server-side/src/main/java/com/jacenterprises/mysteryeducator/MysteryeducatorApplication.java
@@ -0,0 +1,13 @@
+package com.jacenterprises.mysteryeducator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysteryeducatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysteryeducatorApplication.class, args);
+	}
+
+}
---
+++ server-side/src/test/java/com/jacenterprises/mysteryeducator/MysteryeducatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jacenterprises.mysteryeducator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysteryeducatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
