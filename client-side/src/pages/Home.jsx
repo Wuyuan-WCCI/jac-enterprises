def0798f99d66ca576c634d8fdf11c9a6fb2eb2d
Mike <michaelvythilengam@hotmail.com>
2023-09-01 22:46:43 -0400

    Linker additions

--- client-side/src/pages/Home.jsx
+++ client-side/src/pages/Home.jsx
@@ -1,10 +1,19 @@
-import MikeV from "../img/MikeV.jpeg"
+import { Link } from "react-router-dom";
+import Nasa from "../img/nasa.jpeg"
+import Metro from "../img/metro.png"
+import Card from "../components/ui/Card";
 
 const Home = () => {
     return (
         <>
         <h1>Home of the Mystery Educator</h1>
-        <img src={MikeV} alt="Mike V" />
+        <Card>
+            <Link to="/nasa"><img src={Nasa} alt="Nasa" width={300} height={200}/></Link>
+        </Card>
+        <br/>
+        <Card>
+            <Link to="/metro"><img src={Metro} alt="Nasa" width={300} height={200}/></Link>
+        </Card>
         </>
     )
 }
--- client-side/src/pages/JACEnterprise.jsx
+++ client-side/src/pages/JACEnterprise.jsx
@@ -1,14 +1,20 @@
 import JAC from "../img/JAC.jpeg"
+import Card from "../components/ui/Card";
 
 const JACEnterprise = () => {
   return (
-    <a
-      href="https://github.com/cljenkinsjr/jackr-enterprises-site"
-      target="_blank"
-      rel="noreferrer"
-    >
-    <img src={JAC} alt="JAC"/>
-    </a>
+    <>
+    <br/>
+    <Card>
+      <a
+        href="https://github.com/cljenkinsjr/jackr-enterprises-site"
+        target="_blank"
+        rel="noreferrer"
+      >
+      <img src={JAC} alt="JAC"/>
+      </a>
+    </Card>
+    </>
   );
 };
 
